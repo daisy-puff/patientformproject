04ff4e7a19c0bac4dad20ddef2df4e5afca9442a
daisy-puff <baloch.sana@outlook.com>
2025-04-03 16:33:40 -0500

    Update hw2.js

--- hw2.js
+++ hw2.js
@@ -393,19 +393,58 @@ function validateEverything()
 {
      let valid = true;
 
-     if (!validateFname()) valid = false;
-     if (!validateMidint()) valid = false;
-     if (!validateLname()) valid = false;
-     if (!validateDob()) valid = false;
-     if (!validateSsn()) valid = false;
-     if (!validateAddress1()) valid = false;
-     if (!validateZipcode()) valid = false;
-     if (!validateEmail()) valid = false;
-     if (!validatePhone()) valid = false;
-     if (!validateFname()) valid = false;
-     if (!validateUserid()) valid = false;
-     if (!validatePassword()) valid = false;
-     if (!validatePassword2()) valid = false;
+     if (!validateFname()) 
+     {
+           valid = false;
+     }
+     if (!validateMidint()) 
+     {
+           valid = false;
+     }
+     if (!validateLname())
+     {
+           valid = false;
+     }
+     if (!validateDob())
+     {
+           valid = false;
+     }
+     if (!validateSsn())
+     {
+           valid = false;
+     }
+     if (!validateAddress1())
+     {
+           valid = false;
+     }
+     if (!validateZipcode())
+     {
+           valid = false;
+     }
+     if (!validateEmail())
+     {
+           valid = false;
+     }
+     if (!validatePhone())
+     {
+           valid = false;
+     }
+     if (!validateFname())
+     {
+           valid = false;
+     }
+     if (!validateUserid())
+     {
+           valid = false;
+     }
+     if (!validatePassword())
+     {
+           valid = false;
+     }
+     if (!validatePassword2())
+     {
+           valid = false;
+     }
 
      if (valid)
      {
